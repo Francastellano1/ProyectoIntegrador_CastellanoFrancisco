3418a69bd14100464effb2086cbcd2b7652f4e75
Francastellano1 <francastellano89@gmail.com>
2022-07-26 14:47:31 -0300

    back funciona con postman, ahora se implementa al front...

--- proy_int/BackEnd/src/main/java/com/portafolio/fc/Controller/PersonaController.java
+++ proy_int/BackEnd/src/main/java/com/portafolio/fc/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portafolio.fc.Entity.Persona;
 import com.portafolio.fc.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,12 +27,14 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
@@ -39,6 +42,7 @@ public class PersonaController {
     }
     //asi queda la url:
     //URL:PUERTO/personas/editar/id/nombre & apellido & img
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
